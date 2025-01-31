1274153516838f8ca21f11ab33b8e848c20a6879
hyeji-neee <tlsgmlsk123@naver.com>
2025-01-31 21:04:18 +0900

    feat: 학생 목록을 관리하는 Students 클래스 생성, ArrayList 선언 및 생성자 작성

---
+++ src/main/java/com/example/java/domains/student/Students.java
@@ -0,0 +1,11 @@
+package com.example.java.domains.student;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Students {
+    private List<Student> stdList;
+
+    public Students() {
+        stdList = new ArrayList<>();
+    }
+}
