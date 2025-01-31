3fb6b84046d819a81bfcfe3c422ccbfbb3aad7fe
hyeji-neee <tlsgmlsk123@naver.com>
2025-01-31 20:37:27 +0900

    feat: 수정 화면을 출력하는 modifyMenu 함수 작성

--- src/main/java/com/example/java/view/OutputView.java
+++ src/main/java/com/example/java/view/OutputView.java
@@ -15,6 +15,11 @@ public class OutputView {
         System.out.print("메뉴를 선택하세요 : ");
     }
 
+    public void modifyMenu(){
+        System.out.println("1. 학번, 2. 이름, 3. 국어 점수, 4. 수학 점수, 5. 영어 점수");
+        System.out.print("수정할 항목을 선택하세요 : ");
+    }
+
     public void showStudentIdPrompt(){
         System.out.print("학번 8자리를 입력하세요 : ");
     }
