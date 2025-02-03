9f8d370e32bbbbccd971a113b9363c6df1733dd9
hyeji-neee <tlsgmlsk123@naver.com>
2025-02-03 10:28:01 +0900

    refactor: Student 객체를 초기화하는 코드 삭제(등록 기능만 하도록)

--- src/main/java/com/example/java/domains/function/Register.java
+++ src/main/java/com/example/java/domains/function/Register.java
@@ -4,13 +4,8 @@ import com.example.java.domains.student.Student;
 import java.util.List;
 
 public class Register {
-    Student student;
-    public Register(String id, String name, int korScore, int mathScore, int englishScore){
-        student = new Student(id, name, korScore, mathScore, englishScore);
-    }
-
-    public void stdRegister(List<Student> students) {
-        students.add(this.student);
+    public void stdRegister(List<Student> students, Student student) {
+        students.add(student);
         System.out.println("학생 등록이 완료되었습니다!");
     }
 }
