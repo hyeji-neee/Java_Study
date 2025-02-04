7048e41c68667c7883231b440d5a03acea37546b
hyeji-neee <tlsgmlsk123@naver.com>
2025-02-04 09:22:44 +0900

    refactor: 검증을 관리하는 클래스 생성 후 검증 함수 이동

---
+++ src/main/java/com/example/java/domains/function/Validation.java
@@ -0,0 +1,21 @@
+package com.example.java.domains.function;
+
+import com.example.java.domains.student.Student;
+
+import java.util.List;
+
+public class Validation {
+    public void validateSid(String sid) {
+        if(sid.length() != 8) {
+            throw new IllegalArgumentException("학번은 8자리이어야 합니다.");
+        }
+    }
+
+    public void validateRegistration(List<Student> students, String sid) {
+        for (Student s : students) {
+            if (s.getSid().equals(sid)) {
+                throw new IllegalArgumentException("이미 등록되어있는 학생입니다.");
+            }
+        }
+    }
+}
--- src/main/java/com/example/java/domains/student/Student.java
+++ src/main/java/com/example/java/domains/student/Student.java
@@ -40,23 +40,5 @@ public class Student {
         return Math.round((koreanScore + mathScore + englishScore)/3.0);
     }
 
-    public void validate(String sid) {
-        validateSid(sid);
-    }
 
-    private void validateSid(String sid) {
-        if(sid.length() != 8) {
-            throw new IllegalArgumentException("학번은 8자리이어야 합니다.");
-        }
-    }
-
-    public void validateStudentRegistration(List<Student> students, Student student) {
-        validateExistingStudent(students, student);
-    }
-
-    private void validateExistingStudent(List<Student> students, Student student){
-        if(students.contains(student)){
-            throw new IllegalArgumentException("이미 등록되어있는 학생입니다.");
-        }
-    }
 }
