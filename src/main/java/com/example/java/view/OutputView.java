ecacc7ed20808a1b4f6ad2ea3af38344a543b900
hyeji-neee <tlsgmlsk123@naver.com>
2025-02-05 11:08:12 +0900

    style: 개행 문자 추가

--- src/main/java/com/example/java/view/OutputView.java
+++ src/main/java/com/example/java/view/OutputView.java
@@ -3,7 +3,7 @@ package com.example.java.view;
 import com.example.java.domains.student.*;
 
 public class OutputView {
-    public void mainMenu(){
+    public void mainMenu() {
         System.out.println("<학생 관리 프로그램>");
         System.out.println("1. 모든 학생 조회 (이름순)");
         System.out.println("2. 모든 학생 조회 (평균순)");
@@ -15,40 +15,44 @@ public class OutputView {
         System.out.print("메뉴를 선택하세요 : ");
     }
 
-    public void modifyMenu(){
+    public void modifyMenu() {
         System.out.println("1. 학번, 2. 이름, 3. 국어 점수, 4. 수학 점수, 5. 영어 점수");
         System.out.print("수정할 항목을 선택하세요 : ");
     }
 
-    public void showStudentIdPrompt(){
+    public void showStudentIdPrompt() {
         System.out.print("학번 8자리를 입력하세요 : ");
     }
-    public void showStudentNamePrompt(){
+
+    public void showStudentNamePrompt() {
         System.out.print("학생의 이름을 입력하세요 : ");
     }
-    public void showStudentKoreanScorePrompt(){
+
+    public void showStudentKoreanScorePrompt() {
         System.out.print("국어 점수를 입력하세요 (0~100) : ");
     }
-    public void showStudentMathScorePrompt(){
+
+    public void showStudentMathScorePrompt() {
         System.out.print("수학 점수를 입력하세요 (0~100) : ");
     }
-    public void showStudentEnglishScorePrompt(){
+
+    public void showStudentEnglishScorePrompt() {
         System.out.print("영어 점수를 입력하세요 (0~100) : ");
     }
 
-    public void showDeleteStudentIdPrompt(){
-        System.out.print("삭제할 학생의 학번을 입력하세요 : ");
+    public void showDeleteStudentIdPrompt() {
+        System.out.print("\n삭제할 학생의 학번을 입력하세요 : ");
     }
 
-    public void showSearchStudentIdPrompt(){
-        System.out.print("검색할 학생의 학번을 입력하세요 : ");
+    public void showSearchStudentIdPrompt() {
+        System.out.print("\n검색할 학생의 학번을 입력하세요 : ");
     }
 
-    public void showRegistrationSuccessMessage(){
-        System.out.println("학생 등록이 완료되었습니다!");
+    public void showRegistrationSuccessMessage() {
+        System.out.println("\n학생 등록이 완료되었습니다!\n");
     }
 
-    public void showDeletionSuccessMessage(String deletedName){
-        System.out.println(deletedName + " 님이 삭제되었습니다.");
+    public void showDeletionSuccessMessage(String deletedName) {
+        System.out.println("\n" + deletedName + " 님이 삭제되었습니다.\n");
     }
 }
